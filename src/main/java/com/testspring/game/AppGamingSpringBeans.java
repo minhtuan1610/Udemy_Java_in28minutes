d251a7a4d1895fb312f02a8e84def8cd652a057d
minhtuan <tuannm1610@gmail.com>
2023-03-12 23:59:06 +0700

    Section 26: Using @Component and @ComponentScan

--- src/main/java/com/testspring/game/AppGamingSpringBeans.java
+++ src/main/java/com/testspring/game/AppGamingSpringBeans.java
@@ -1,10 +1,15 @@
 package com.testspring.game;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan("com.testspring.game") // Tell Spring where to scan component for creating Bean
 public class AppGamingSpringBeans {
     public static void main(String[] args) {
-        try (var context = new AnnotationConfigApplicationContext(AppGamingConfiguration.class)) {
+        try (var context = new AnnotationConfigApplicationContext(AppGamingSpringBeans.class)) {
             context.getBean(GamingConsole.class).up();
             context.getBean(GameRunner.class).run();
         }
